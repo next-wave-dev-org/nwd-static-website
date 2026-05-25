0c1de412411d396619944d530e2e45f3a02b52d7
luwamhabtu07 <luwamhabtu07@gmail.com>
2026-05-24 19:05:16 -0700

    feat: add active footer navigation highlighting

--- src/components/microComponents/footer/footerLink.js
+++ src/components/microComponents/footer/footerLink.js
@@ -1,10 +1,20 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 export function FooterLink(props) {
+  const location = useLocation();
+
+  const linkPath = "/" + props.name.toLowerCase();
+
+  const isActive = location.pathname === linkPath;
+
   return (
     <Link
-      style={{ color: "white", textDecoration: "none" }}
-      to={"/" + props.name}
+      style={{
+        color: isActive ? "#ffd700" : "white",
+        textDecoration: isActive ? "underline" : "none",
+        fontWeight: isActive ? "600" : "200",
+      }}
+      to={linkPath}
     >
       {props.name}
     </Link>
